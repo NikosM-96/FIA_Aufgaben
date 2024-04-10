5aadc1c4a461ac3c4db2aa5678bbd6dd35b64dae
NikosM-96 <160034077+NikosM-96@users.noreply.github.com>
2024-04-10 14:29:26 +0200

    Update Program.cs

--- ConsoleAuto/Program.cs
+++ ConsoleAuto/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace ConsoleAuto
+﻿using System.Runtime.InteropServices.Marshalling;
+
+namespace ConsoleAuto
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
         }
     }
 }
